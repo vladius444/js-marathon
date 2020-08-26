a9be3646ee4676044eca207c8e764f19eda90840
Vladislav Rykovskiy <v.rykovskiy@zavod.games>
2020-08-27 00:31:33 +0300

    refac hp properties

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const $btnAttackEnemy = document.getElementById('btn-kick-enemy')
 
 const character = {
     name: 'Pikachu',
-    defaultHP: 200,
-    damageHP: 200,
     maxDamageHP: 25,
+    hp: {
+        current: 200,
+        total: 200,
+    },
     elHP: document.getElementById('health-character'),
     elProgressbar: document.getElementById('progressbar-character'),
     changeHP,
@@ -13,8 +15,10 @@ const character = {
 
 const enemy = {
     name: 'Charmander',
-    defaultHP: 100,
-    damageHP: 100,
+    hp: {
+        current: 100,
+        total: 100,
+    },
     maxDamageHP: 23,
     elHP: document.getElementById('health-enemy'),
     elProgressbar: document.getElementById('progressbar-enemy'),
@@ -22,16 +26,19 @@ const enemy = {
 }
 
 function renderHP() {
-    this.elHP.innerText = this.damageHP + '/' + this.defaultHP
-    this.elProgressbar.style.width = (this.damageHP / this.defaultHP) * 100  + '%'
+    this.elHP.innerText = this.hp.current + '/' + this.hp.total
+    this.elProgressbar.style.width = (this.hp.current / this.hp.total) * 100  + '%'
 }
 
 function changeHP(count) {
-    if (this.damageHP < count) {
-        this.damageHP = 0
+    this.hp.current -= count
+
+    const battleLog = this === enemy ? generateBattleLog(this, character, count) : generateBattleLog(this, enemy, count)
+    console.log(battleLog)
+
+    if (this.hp.current < 0) {
+        this.hp.current = 0
         finishGame(this.name)
-    } else {
-        this.damageHP -= count
     }
 
     renderHP.call(this)
@@ -68,3 +75,22 @@ function finishGame(name) {
 }
 
 initGame()
+
+function generateBattleLog(firstPerson, secondPerson, damage) {
+    const damageStat = `-${damage}, [${firstPerson.hp.current}/${firstPerson.hp.total}]`
+
+    const logs = [
+        `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. ${damageStat}`,
+        `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. ${damageStat}`,
+        `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. ${damageStat}`,
+        `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. ${damageStat}`,
+        `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. ${damageStat}`,
+        `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. ${damageStat}`,
+        `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. ${damageStat}`,
+        `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника. ${damageStat}`,
+        `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. ${damageStat}`,
+        `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. ${damageStat}`
+    ];
+
+    return logs[random(logs.length) - 1]
+}
